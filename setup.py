587f27633611af9131314ed196655867d2285c1e
khaledk2 <khaledk2@hotmail.com>
2024-10-30 18:44:21 +0000

    add licence

--- setup.py
+++ setup.py
@@ -23,10 +23,15 @@ setup(
     description="metadata_model.IDR",
     long_description=read("README.md"),
     classifiers=[
+        "Intended Audience :: Science/Research",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-    ],
+    ],  # Get strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    author="The Open Microscopy Team",
+    author_email="ome-devel@lists.openmicroscopy.org.uk",
+    url="https://github.com/khaledk2/idr_metadata_model",
     packages=find_packages(exclude=("test",)),
     python_requires=">=3.9",
     install_requires=[
@@ -34,8 +39,9 @@ setup(
     "linkml-runtime==1.8.0"
     ],
     include_package_data=True,
+    license = "Apache-2.0",
 
 )
 
-
+# python setup.py bdist_wheel
 
